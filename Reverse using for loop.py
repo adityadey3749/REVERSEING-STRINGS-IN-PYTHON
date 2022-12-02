9eb57f7e1ea09be257e3d4a477ac27dc8f80887d
Aditya Dey <82680373+adityadey3749@users.noreply.github.com>
2022-12-02 13:33:44 +0530

    Create Reverse using for loop.py

---
+++ Reverse using for loop.py
@@ -0,0 +1,10 @@
+#REVERSING STRINGS USING FOR LOOP
+
+def reverse_string(j):
+    j1 = ''
+    for s in j:
+        j1 = s + j1
+    return j1
+        
+input_string = 'SHUBHAM'
+print("reverse of string using for loop is : ",reverse_string(input_string))
